d135733c7bfaadf5add6eca9b17710b48d39fed1
Ayush Dwivedi <amayush0818@gmail.com>
2025-04-15 00:49:10 +0530

    Completed Hero Section

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,32 +14,31 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider appearance={{
-      baseTheme: dark
-    }}>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en" suppressHydrationWarning>
         <head>
           <link rel="icon" href="/logo.png" sizes="any" />
         </head>
-        <body
-          className={`${inter.className}`}
-        >
+        <body className={`${inter.className}`}>
           <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-            >
-              {/* header  */}
-              <Header />
-              <main className="min-h-screen">{children}</main>
-              {/* footer  */}
-              <footer className="bg-muted/50 py-12">
-                <div className="container mx-auto px-4 text-center text-gray-200">
-                  <p>Made with ❤️ by Code Therapist!</p>
-                </div>
-              </footer>
-            </ThemeProvider>
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with ❤️ by Code Therapist!</p>
+              </div>
+            </footer>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
