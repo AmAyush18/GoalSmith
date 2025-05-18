7c71f86319f9d1af29c57fcc2302a268389cb4c6
Ayush Dwivedi <amayush0818@gmail.com>
2025-05-18 19:00:11 +0530

    Completed Onboarding Form and use-fetch hook

---
+++ src/app/(main)/dashboard/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { redirect } from 'next/navigation';
+import { getUserOnboardingStatus } from '../../../../actions/user';
+
+const IndustryInsightsPage = async () => {
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  if (!isOnboarded) {
+    redirect("/onboarding")
+  }
+  return (
+    <div>IndustryInsightsPage</div>
+  )
+}
+
+export default IndustryInsightsPage
